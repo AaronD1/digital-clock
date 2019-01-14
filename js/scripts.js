75239f9cf991b0659d12a29ea0a5dd2e8ca36527
Aaron <aarondubois@outlook.com>
2019-01-14 07:05:24 -0800

    Fulldate + Flexbox
    Added the full date and restructured the CSS with Flexbox.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
+const daysOfTheWeek = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
+const months = ["January","February","March","April","May","June","July",
+"August","September","October","November","December"];
+
 $(document).ready(function() {
+
+    function displayDay() {
+        let newDay = new Date();
+        let dayIndex = newDay.getDay();
+        let day = daysOfTheWeek[dayIndex];
+        const dayDiv = $('.day');
+        dayDiv.text(day);
+
+    }
+
+    function displayDate() {
+        // Get month
+        let newMonth = new Date();
+        let monthIndex = newMonth.getMonth();
+        let month = months[monthIndex];
+        // Get day
+        let newDate = new Date();
+        let day = newDate.getDate();
+        // Get year
+        let newYear = new Date();
+        let year = newYear.getFullYear();
+        let dateDiv = $('.date');
+        // Update html with month, day, year
+        dateDiv.text(month.toUpperCase() + " " + day + " " + year)
+    }
 
     function displayTime() {
         let currentTime = new Date();
@@ -24,12 +53,17 @@ $(document).ready(function() {
             minutes = "0" + minutes;
         }
 
-        const clockDiv = $('.clock');
-        clockDiv.text(hours + ":" + minutes + ":" + seconds + " " + meridiem);
+        const timeDiv = $('.time');
+        timeDiv.text(hours + ":" + minutes + ":" + seconds + " " + meridiem);
     }
 
+    displayDay();
     displayTime();
-    setInterval(displayTime, 1000);
+    setInterval(function() {
+        displayDay();
+        displayDate();
+        displayTime();
+    }, 1000);
 
 });
 
