6864c95041ee1bedffbbcc168c8832024c94a8f6
Aaron <aarondubois@outlook.com>
2019-01-14 04:41:33 -0800

    Setup clock. Reorganize file structure.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    function displayTime() {
+        let currentTime = new Date();
+        let hours = currentTime.getHours();
+        let minutes = currentTime.getMinutes();
+        let seconds = currentTime.getSeconds();
+
+        const clockDiv = $('.clock');
+        clockDiv.text(hours + ":" + minutes + ":" + seconds);
+    }
+
+    displayTime();
+
+});
+
