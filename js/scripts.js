507e190d17ca416af0241de8167772096fa9302f
Aaron <aarondubois@outlook.com>
2019-01-14 04:59:46 -0800

    Update scripts.js
    All numbers will remain double-digits at all times.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,18 @@ $(document).ready(function() {
         let minutes = currentTime.getMinutes();
         let seconds = currentTime.getSeconds();
 
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+
         const clockDiv = $('.clock');
         clockDiv.text(hours + ":" + minutes + ":" + seconds);
     }
